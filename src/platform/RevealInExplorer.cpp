ef916ec4ecb5e33d6798b58893dd7b977cc2b17c
Hunlongyu <hunlongyu@gmail.com>
2026-07-04 16:40:42 +0800

    fix(open): log explorer launch failures; style help groups + preview info

--- src/platform/RevealInExplorer.cpp
+++ src/platform/RevealInExplorer.cpp
@@ -1,4 +1,5 @@
 #include "platform/RevealInExplorer.h"
+#include <QtGlobal>
 #include <QFileInfo>
 #include <QDir>
 #include <QProcess>
@@ -14,11 +15,13 @@ bool revealInExplorer(const QString& path) {
 
 #if defined(Q_OS_WIN)
     const QString native = QDir::toNativeSeparators(fi.absoluteFilePath());
-    if (fi.isDir())
-        return QProcess::startDetached(QStringLiteral("explorer"), {native});
     // "/select," must reach explorer as a single argument glued to the path.
-    return QProcess::startDetached(QStringLiteral("explorer"),
+    const bool ok = fi.isDir()
+        ? QProcess::startDetached(QStringLiteral("explorer"), {native})
+        : QProcess::startDetached(QStringLiteral("explorer"),
                                    {QStringLiteral("/select,") + native});
+    if (!ok) qWarning("revealInExplorer: failed to launch explorer for %s", qPrintable(native));
+    return ok;
 #else
     const QString dir = fi.isDir() ? fi.absoluteFilePath() : fi.absolutePath();
     return QDesktopServices::openUrl(QUrl::fromLocalFile(dir));
